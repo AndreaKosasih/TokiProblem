92f154d8a7a41a82c0be87dce2aa9e92f4388ae8
AndreasKosasih <andreastegusks@gmail.com>
2023-10-03 18:00:00 +0700

    PKD

---
+++ Runtuh.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int R, C; cin >> R >> C; 
+    int array[R][C];
+    for(int i = 1; i < R; i++) {
+        string a; cin >> a; 
+        for(int j = 0; j < C; i++) {
+            if(a[j]=='0') {
+                array[i][j] = 0; 
+            } else {
+                array[i][j] = 1;
+            }
+        }
+    }
+    bool done = true;
+    while(!done) {
+        done = true; 
+        int fullB = R; 
+        for(int i = R - 1; i >= 0; i--){
+            bool full = true; 
+            for(int j = 0; j < C; j++) {
+                if(array[i][j]==0){
+                    full = false; 
+                    break;
+                }
+            }
+            if(full) {
+                done = false; 
+                if(fullB==R) fullB = i;
+                for(int j = 0; j < C; j++) array[i][j] = 0; 
+            }
+        }
+        if (!done) {
+            int atas[C];
+            for(int i = 0; i < C; i++) {
+                atas[i]=0; 
+                for(int j = 0; j < fullB; j++) {
+                    atas[i] = atas[i] + array[j][i];
+                    array[j][i] = 0 ; 
+                }
+                int bawah = fullB; 
+                while(array[bawah + 1][i]==0 && bawah < R - 1) bawah++;
+                for(int j = 0; j < atas[i]; j++) array[bawah - 1][i] = 1; 
+            }
+        }
+        for(int i = 0; i < R; i++) {
+            for(int j = 0; j < C; j++) {
+                cout << array[i][j];;
+            }
+            cout << endl;
+        }
+    }
+    return 0; 
+}
+ 
